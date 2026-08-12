4f6b842640c2ae3ba322428755a6876aa39678c2
ataha987-wq <a.taha987@gmail.com>
2026-08-12 09:06:50 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// حط هنا رابط تطبيق Google Apps Script بعد النشر (Deploy > New deployment > Web app)
+// شكل الرابط بيكون زي كده:
+// https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXXXXXXXXX/exec
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyrA9YjF7YKq8x0aRx73twMBfyComHGbmu8ECZ43luGvlfzl5rjQMoOygIeUOE7wnP9/exec';
+
+// نفس كلمتي المرور المضبوطتين في Code.gs (لو غيّرتهم هناك، لازم تغيّرهم هنا بالظبط برضه)
+const EMPLOYEE_PASSWORD = '2020';
+const ADMIN_PASSWORD = '1839';
