bc77933cdbd6f51c67a02635ddfc838361c9b504
ataha987-wq <a.taha987@gmail.com>
2026-08-12 09:06:34 +0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// حط هنا رابط تطبيق Google Apps Script بعد النشر (Deploy > New deployment > Web app)
-// شكل الرابط بيكون زي كده:
-// https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXXXXXXXXX/exec
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyrA9YjF7YKq8x0aRx73twMBfyComHGbmu8ECZ43luGvlfzl5rjQMoOygIeUOE7wnP9/exec';
